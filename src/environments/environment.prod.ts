c2c43be28bcbe8806b4f387ada19fbcdf187a343
Aenarin01 <vladyslav.hurinchuk@nure.ua>
2022-01-12 11:24:11 +0200

    first

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  apiKey: 'AIzaSyDb97pUoHRcjAztEdCgNY5qCxQXP6YVumY',
+  fbDbUrl: 'https://shop-1305-default-rtdb.firebaseio.com/'
 };
